64baf7274d2153cb163cb1eb6ce026ae97e63cc3
arumraa <113223271+arumraa@users.noreply.github.com>
2022-09-15 22:00:55 +0700

    tugas cod suhu

---
+++ araaaaa.py
@@ -0,0 +1,19 @@
+print("Arum Rahmadhani")
+print("220441100074")
+print("lab TI")
+
+print("Selamat Datang Di Program Arumra :) ")
+print("masukkan suhu celcius yang akan di konversi")
+suhu = int(input())
+x = suhu
+a = suhu
+b = suhu
+suhu = float(suhu * 9) /5 + 32
+print(str(b) + " celcius adalah  " + str(suhu) + "  fahrenhait")
+suhu = x + 273
+print(str(b) + " celcius adalah  " + str(suhu) + "  kelvin")
+suhu = float(a * 4)/5
+print(str(b) + " celcius adalah  " + str (suhu)+ "  reamur")
+print("....................................")
+print("              terimakasih           ")
+print("....................................")
